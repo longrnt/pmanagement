f5338f5f78224230f92968d8b0ecc904ea092aef
Long Tran <tranlong204@gmail.com>
2018-10-31 22:35:15 -0700

    first commit

---
+++ src/main/java/com/codinhub/pmanagement/PmanagementApplication.java
@@ -0,0 +1,12 @@
+package com.codinhub.pmanagement;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PmanagementApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PmanagementApplication.class, args);
+    }
+}
